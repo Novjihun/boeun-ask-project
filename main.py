ebb915a02168385d0a13d8fc83e9a234e353054a
Novjihun <166613971+Novjihun@users.noreply.github.com>
2024-07-16 15:20:22 +0900

    Add files via upload

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, url_for, flash
+from flask import Flask, render_template, request, redirect, url_for, flash, session
 import os
 
 app = Flask(__name__)
@@ -23,14 +23,16 @@ def sign_in():
     username = request.form.get("username")
     password = request.form.get("password")
     if check_credentials(username, password):
+        session['logged_in'] = True
+        session['username'] = username
         return redirect(url_for('main'))  # 로그인 성공 시 메인 페이지로 리다이렉트
     else:
-        flash("Invalid credentials. Please try again.")
+        flash("로그인 정보가 일치하지 않습니다. 다시 시도해주세요.")
         return redirect(url_for('login'))
 
 @app.route('/')
 def main():
-    return render_template('main.html')
+    return render_template('main.html', login=session.get('logged_in'), username=session.get('username'))
 
 @app.route('/login')
 def login():
@@ -43,6 +45,18 @@ def leader_board():
 @app.route('/information')
 def information():
     return render_template('information.html')
+
+@app.route('/logout')
+def logout():
+    session.pop('logged_in', None)
+    session.pop('username', None)
+    return redirect(url_for('main'))
+
+@app.route('/my_info')
+def my_info():
+    if not session.get('logged_in'):
+        return redirect(url_for('login'))
+    return render_template('my_info.html', username=session.get('username'))
 
 if __name__ == '__main__':
     app.run(debug=True)
